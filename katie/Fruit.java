620b3651781578b8ba0ff1365ff47f60f5820468
dwj300 <dwj300@gmail.com>
2013-10-05 17:38:15 -0400

    adding java demo

---
+++ katie/Fruit.java
@@ -0,0 +1,15 @@
+public class Fruit {
+    public static String name = "Fruit";
+    // Instance variables
+    public String color;
+    public int size;
+    private boolean tastesGood;
+
+    public void setTastesGood(boolean tastesGood) {
+        this.tastesGood = tastesGood;
+     }
+
+    public boolean getTastesGood() {
+        return tastesGood;
+    }
+}
---
+++ katie/Hello.java
@@ -0,0 +1,7 @@
+// Hello world
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Katie");
+    }
+}
